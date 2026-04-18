81e919f4d209dd8fcef20437998a030075896fb1
Just_Favor <vivasfavor@gmail.com>
2026-04-18 22:46:35 +0100

    bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full bg-gray-50">
+      <body className="min-h-full">
       <Toaster position="bottom-left" />
         <NextAuthProvider>
         <AuthProvider>
